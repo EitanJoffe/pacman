c3a5ceb6899201c1c058ea28c988883bbee546b1
naomifrid <naomiki.fridman@gmail.com>
2026-01-14 19:09:15 +0200

    added main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import arcade
+from constants import WINDOW_WIDTH, WINDOW_TITLE, WINDOW_HEIGHT
+from game import PacmanGame
 """
 נקודת הכניסה למשחק פקמן.
 
@@ -9,8 +12,13 @@
 """
 
 def main():
-    """פונקציית main שמריצה את המשחק."""
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    game = PacmanGame()
+    game.setup()
+    window.show_view(game)
+    arcade.run()
 
+    """פונקציית main שמריצה את המשחק."""
 
 
 if __name__ == "__main__":
