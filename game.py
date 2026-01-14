f33c948df6848cd1c792f6c32185c43ae2d61c17
eva-nitzanim <eva.poluliakhov@nitzanim.tech>
2026-01-14 17:56:01 +0200

    added init function to pacmanGame class and also the on_draw function to the same class

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from constants import LEVEL_MAP, TILE_SIZE
+from constants import LEVEL_MAP, TILE_SIZE,WINDOW_WIDTH,WINDOW_HEIGHT
 import arcade
 from characters import Player, Enemy, Coin, Wall
 """
@@ -7,7 +7,21 @@ from characters import Player, Enemy, Coin, Wall
 מכיל את המחלקה:
 - PacmanGame: ניהול מצב המשחק, ציור, עדכון ותשובת מקלדת.
 """
+
+
 class PacmanGame(arcade.View()):
+    def __init__(self):
+        super().__init__()
+        self.background_color = arcade.color.BLACK
+        self.start_x = 0
+        self.start_y = 0
+        self.game_over = False
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.ghost_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
+        self.player=None
+
     def setup(self):
         flag = False
 
@@ -35,4 +49,15 @@ class PacmanGame(arcade.View()):
                     current = Enemy(x,y, 5)
                     ghost_list.append(current)
 
+    def on_draw(self):
+        self.clear()
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.ghost_list.draw()
+        self.player_list.draw()
+        arcade.draw_text(self.player.score,0,WINDOW_HEIGHT,arcade.color.YELLOW,TILE_SIZE//2)
+        arcade.draw_text(self.player.lives,0,WINDOW_HEIGHT-TILE_SIZE,arcade.color.RED,TILE_SIZE//2)
 
+        if self.player.lives==0:
+            self.game_over=True
+            arcade.draw_text("You Lost ",WINDOW_WIDTH//2,WINDOW_HEIGHT//2,arcade.color.YELLOW,WINDOW_WIDTH//5)
