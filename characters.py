b4e334c79e1102866d2b00eb71cc1aa41792f900
EitanJoffe <eitanpro4@gmail.com>
2026-01-14 17:03:38 +0200

    Updated charaters to sprites, added in the game.py the setup function to convert the map into ui.

--- characters.py
+++ characters.py
@@ -1,3 +1,7 @@
+import random
+import arcade
+from constants import TILE_SIZE
+
 """
 מודול הדמויות (Sprites) במשחק פקמן.
 
@@ -8,3 +12,65 @@
 - Coin: מטבעות לאיסוף
 - Wall: קירות שחוסמים תנועה
 """
+
+class Character(arcade.Sprite):
+    def _init_(self, center_x, center_y, speed, color):
+        super().__init__()
+        radius = TILE_SIZE // 2 - 2
+        texture = arcade.make_circle_texture(radius * 2, color)
+        self.texture = texture
+        self.center_x = center_x
+        self.center_y = center_y
+        self.width = texture.width
+        self.height = texture.height
+        self.speed = speed
+        self.change_x = 0
+        self.change_y = 0
+
+class Player(Character):
+    def __init__(self, center_x, center_y, speed):
+        super().__init__(center_x, center_y, speed, arcade.color.YELLOW)
+        self.score = 0
+        self.lives = 3
+
+    def move(self):
+        self.center_x = self.change_x * self.speed
+        self.center_y = self.change_y * self.speed
+
+class Enemy(Character):
+    def __init__(self, center_x, center_y, speed):
+        super().__init__(center_x, center_y, speed, arcade.color.RED)
+        self.time_to_change_direction = 0
+
+    def pick_new_direction(self):
+        directions = [(0,1), (0,-1), (1,0), (-1,0), (0,0)]
+        self.change_x, self.change_y = random.choice(directions)
+        self.time_to_change_direction = random.uniform(0.3, 1.0)
+
+    def update_place(self, delta_time = 1 / 60):
+        self.time_to_change_direction -= delta_time
+        if self.time_to_change_direction == 0:
+            self.pick_new_direction()
+            self.center_x = self.change_x * self.speed
+            self.center_y = self.change_y * self.speed
+
+class Coin(arcade.sprite):
+    def __init__(self, center_x, center_y):
+        super().__init__()
+        radius = TILE_SIZE // 2 - 10
+        texture = arcade.make_circle_texture(radius * 2, arcade.color.YELLOW)
+        self.texture = texture
+        self.width = texture.width
+        self.height = texture.height
+        self.center_x = center_x
+        self.center_y = center_y
+        self.value = 10
+
+class Wall(arcade.sprite):
+    def __init__(self, center_x, center_y):
+        texture = arcade.make_soft_square_texture(TILE_SIZE,arcade.color.BLUE,TILE_SIZE // 2)
+        self.texture = texture
+        self.width = texture.width
+        self.height = texture.height
+        self.center_x = center_x
+        self.center_y = center_y
