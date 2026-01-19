3197b42171bd4d988c89daabb1b648d44f5d7a07
EitanJoffe <eitanpro4@gmail.com>
2026-01-20 00:08:49 +0200

    in the characters.py fixed some mistakes, in the enemy class, update_place function, changed the time to change direction if statement to <= 0 and not only == 0, because the time to change direction can also be a number bigger than 0, and not specifically 0. also in the same function moved the center_x and center_y away from the if statement, so the ghost will move freely and not be dependent on the if statement. in the game.py file fixed some errors, in the on key release function, the self.player.change_y and change_x was like change.y and change.x, so fixed that. on the on_update function instead of checking the x and y cords, used the arcade build function check_for_collision,that checks if a collision happens between 2 sprites, if so return True. created a wall collision list between a wall and a ghost to see if the collision between them happens.

--- game.py
+++ game.py
@@ -1,15 +1,17 @@
-from arcade.experimental.shapes_perf import TITLE
-
 from constants import LEVEL_MAP, TILE_SIZE,WINDOW_WIDTH,WINDOW_HEIGHT
 import arcade
 from characters import Player, Enemy, Coin, Wall
+
 """
 מודול הלוגיקה הראשית של משחק הפקמן.
 מכיל את המחלקה:
 - PacmanGame: ניהול מצב המשחק, ציור, עדכון ותשובת מקלדת.
 """
 
-
+"""
+need to change the starting position of the player after a collision with a ghost
+need to fix when you lose the game, that it will reset the board, and not duplicate 2 players and have bugs
+"""
 class PacmanGame(arcade.View):
     def __init__(self):
         super().__init__()
@@ -39,11 +41,11 @@ class PacmanGame(arcade.View):
                     current = Coin(x, y)
                     self.coin_list.append(current)
                 elif current == 'P':
-                    current = Player(x, y, 5)
+                    current = Player(x, y, 2)
                     self.player = current
                     self.player_list.append(current)
                 elif current == 'G':
-                    current = Enemy(x,y, 5)
+                    current = Enemy(x,y, 2)
                     self.ghost_list.append(current)
 
     def on_draw(self):
@@ -57,7 +59,7 @@ class PacmanGame(arcade.View):
 
         if self.player.lives==0:
             self.game_over=True
-            arcade.draw_text("You Lost ",WINDOW_WIDTH//2,WINDOW_HEIGHT//2,arcade.color.YELLOW,WINDOW_WIDTH//5)
+            arcade.draw_text("You Lost ",WINDOW_WIDTH//5,WINDOW_HEIGHT//5,arcade.color.RED,WINDOW_WIDTH//5)
 
     def on_key_press(self,key,modifiers):
         if key==arcade.key.UP:
@@ -69,51 +71,53 @@ class PacmanGame(arcade.View):
         if key==arcade.key.RIGHT:
             self.player.change_x=1
 
-        if self.game_over==True:
+        if self.game_over:
             if key==arcade.key.SPACE:
                 self.setup()
 
+
     def on_key_release(self,key,modifiers):
         if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player.change.y = 0
+            self.player.change_y = 0
         if key == arcade.key.RIGHT or key == arcade.key.LEFT:
-            self.player.change.x = 0
+            self.player.change_x = 0
 
 
     def on_update(self, delta_time):
-        if self.game_over == True:
+        if self.game_over:
             return
 
         current_x = self.player.center_x
         current_y = self.player.center_y
         self.player.move()
-        wall_collision_list = arcade.check_for_collision_with_list(self.player, self.wall_list)
+        wall_collision_with_player_list = arcade.check_for_collision_with_list(self.player, self.wall_list)
 
-        for wall in wall_collision_list:
-            if self.player.center_x == wall.center_x and self.player.center_y == wall.center_y:
-                self.player.center_x, self.player_list.center_y = current_x, current_y
+        for wall in wall_collision_with_player_list:
+            if arcade.check_for_collision(self.player, wall):
+                self.player.center_x, self.player.center_y = current_x, current_y
 
         for ghost in self.ghost_list:
+            wall_collision_with_ghost_list = arcade.check_for_collision_with_list(ghost, self.wall_list)
             ghost_x = ghost.center_x
             ghost_y = ghost.center_y
             ghost.update_place(delta_time)
-            for wall in wall_collision_list:
-                 if ghost.center_y == wall.center_x and ghost.center_y == wall.center_y:
+            for wall in wall_collision_with_ghost_list:
+                 if arcade.check_for_collision(ghost, wall):
                     ghost.center_x = ghost_x
                     ghost.center_y = ghost_y
                     ghost.pick_new_direction()
 
         coin_collision_list = arcade.check_for_collision_with_list(self.player, self.coin_list)
         for coin in coin_collision_list:
-            if self.player.center_x == coin.center_x and self.player.center_y and coin.center_y:
+            if arcade.check_for_collision(self.player, coin):
                 coin.remove_from_sprite_lists()
                 self.player.score += coin.value
 
         ghost_collision_list = arcade.check_for_collision_with_list(self.player, self.ghost_list)
         for ghost in ghost_collision_list:
-            if self.player.center_x == ghost.center_x and self.player.center_y == ghost.center_y:
+            if arcade.check_for_collision(self.player, ghost):
                 self.player.lives -= 1
                 self.player.center_x, self.player.center_y = TILE_SIZE, TILE_SIZE
-                self.player.speed = 1
+                self.player.speed = 2
         if self.player.lives == 0:
             self.game_over = True
