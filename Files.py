2b9446ade6ffddfdaa7d763b7b3e15dc294792ff
Omer <omerhuliano@gmail.com>
2026-01-26 18:14:23 +0200

    add file "data , files , text " to the project

---
+++ Files.py
@@ -0,0 +1,26 @@
+
+
+
+
+# # פתיחת הקובץ text.txt במצב קריאה ('r')
+# with open("text.txt", "r") as file:
+#     # קריאת כל התוכן מהקובץ
+#     content = file.read()
+#     content = content.split("\n")
+#
+# # הדפסת התוכן שנקרא
+# print(content)
+
+# import csv
+#
+# # פתיחת הקובץ במצב קריאה
+# with open("data.csv", "r") as file:
+#     reader = csv.reader(file)
+#
+#     # לולאה שעוברת על כל שורה בקובץ
+#     for row in reader:
+#         print(row)
+#
+
+
+
