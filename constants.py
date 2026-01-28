1b8a3b2879ddc0b707168f1397c00e580412faf4
Omer <omerhuliano@gmail.com>
2026-01-28 18:00:56 +0200

    made all the constants  come from the data file

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
+import csv
+
+
+
 """
 מודול קבועים עבור משחק פקמן.
 
@@ -11,14 +15,32 @@ LEVEL_MAP=[]
 with open("text.txt","r") as file:
     for row in file:
         LEVEL_MAP.append(row.strip())
-print(LEVEL_MAP)
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-WINDOW_TITLE = "Pacman Arcade Example"
+
+with open ("data.csv","r")as file:
+    num_of_constants=0
+    reader=csv.reader(file)
+    constants_items=[]
+    for row in reader:
+        for i in range(len(row)):
+            num_of_constants+=1
+            constants_items.append(row[i])
+    for j in range(num_of_constants//2):
+        if constants_items[j]== "WINDOW_WIDTH":
+            WINDOW_WIDTH=int(constants_items[j + num_of_constants //2])
+        if constants_items[j]== "WINDOW_HEIGHT":
+            WINDOW_HEIGHT=int(constants_items[j + num_of_constants //2])
+        if constants_items[j]== "WINDOW_TITLE":
+            WINDOW_TITLE=str(constants_items[j + num_of_constants //2])
+        if constants_items[j]== "TILE_SIZE":
+            TILE_SIZE=int(constants_items[j + num_of_constants // 2])
+
+#WINDOW_WIDTH = 800
+#WINDOW_HEIGHT = 600
+#WINDOW_TITLE = "Pacman Arcade Example"
 
 # גודל אריח במפה (בפיקסלים)
-TILE_SIZE = 32
+#TILE_SIZE = 32
 
 # מפה:
 # # - קיר
